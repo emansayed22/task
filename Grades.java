d0ec67d6cb82f84bcff10e14d7b95ffb747c08dd
emansayed22 <73968167+emansayed22@users.noreply.github.com>
2020-12-13 11:51:46 +0200

    Add files via upload

---
+++ Grades.java
@@ -0,0 +1,57 @@
+
+public class Grades {
+	private String name;
+	private double mark;
+	
+	
+	public Grades() {
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public Grades(String name, double mark) {
+		super();
+		this.name = name;
+		this.mark = mark;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getMark() {
+		return mark;
+	}
+	public void setMark(double mark) {
+		this.mark = mark;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
